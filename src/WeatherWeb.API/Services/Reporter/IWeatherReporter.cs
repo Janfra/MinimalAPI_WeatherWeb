6fcffde45889372c5d0392ad1d18e97c8e8b5f7d
Jan <janfrancoherrera@gmail.com>
2026-02-02 10:04:49 +0000

    fix: fix added missing optional token cancellation

--- src/WeatherWeb.API/Services/Reporter/IWeatherReporter.cs
+++ src/WeatherWeb.API/Services/Reporter/IWeatherReporter.cs
@@ -4,8 +4,8 @@ using WeatherWeb.Models;
 
 public interface IWeatherReporter
 {
-    public Task<IReadOnlyList<WeatherReport>> GetHotReportsAsync(IQueryable<WeatherReport> reports);
-    public Task<IReadOnlyList<WeatherReport>> GetLocationReportsAsync(IQueryable<WeatherReport> reports, string location);
-    public Task<IReadOnlyList<string>> GetFormattedReportsAsync(IQueryable<WeatherReport> reports);
-    public Task<IReadOnlyList<WeatherReport>> GetMinHumidityReportsAsync(IQueryable<WeatherReport> reports, float humidityThreshold);
+    public Task<IReadOnlyList<WeatherReport>> GetHotReportsAsync(IQueryable<WeatherReport> reports, CancellationToken cancellationToken = default);
+    public Task<IReadOnlyList<WeatherReport>> GetLocationReportsAsync(IQueryable<WeatherReport> reports, string location, CancellationToken cancellationToken = default);
+    public Task<IReadOnlyList<string>> GetFormattedReportsAsync(IQueryable<WeatherReport> reports, CancellationToken cancellationToken = default);
+    public Task<IReadOnlyList<WeatherReport>> GetMinHumidityReportsAsync(IQueryable<WeatherReport> reports, float humidityThreshold, CancellationToken cancellationToken = default);
 }
--- src/WeatherWeb.API/Services/Reporter/WeatherReporter.cs
+++ src/WeatherWeb.API/Services/Reporter/WeatherReporter.cs
@@ -7,25 +7,26 @@ using WeatherWeb.Services.Formatter;
 
 public class WeatherReporter(IWeatherFormatter formatter) : IWeatherReporter
 {
+    public const int HOT_THRESHOLD_DEGREES = 30;
     private readonly IWeatherFormatter _formatter = formatter;
 
-    public async Task<IReadOnlyList<string>> GetFormattedReportsAsync(IQueryable<WeatherReport> reports)
+    public async Task<IReadOnlyList<string>> GetFormattedReportsAsync(IQueryable<WeatherReport> reports, CancellationToken cancellationToken = default)
     {
-        return await reports.Select(report => _formatter.Format(report)).ToListAsync();
+        return await reports.Select(report => _formatter.Format(report)).ToListAsync(cancellationToken);
     }
 
-    public async Task<IReadOnlyList<WeatherReport>> GetHotReportsAsync(IQueryable<WeatherReport> reports)
+    public async Task<IReadOnlyList<WeatherReport>> GetHotReportsAsync(IQueryable<WeatherReport> reports, CancellationToken cancellationToken = default)
     {
-        return await reports.Where(report => report.TemperatureC >= 30).ToListAsync();
+        return await reports.Where(report => report.TemperatureC >= HOT_THRESHOLD_DEGREES).ToListAsync(cancellationToken);
     }
 
-    public async Task<IReadOnlyList<WeatherReport>> GetLocationReportsAsync(IQueryable<WeatherReport> reports, string location)
+    public async Task<IReadOnlyList<WeatherReport>> GetLocationReportsAsync(IQueryable<WeatherReport> reports, string location, CancellationToken cancellationToken = default)
     {
-        return await reports.Where(report => report.Location.Equals(location)).ToListAsync();
+        return await reports.Where(report => report.Location.Equals(location)).ToListAsync(cancellationToken);
     }
 
-    public async Task<IReadOnlyList<WeatherReport>> GetMinHumidityReportsAsync(IQueryable<WeatherReport> reports, float humidityThreshold)
+    public async Task<IReadOnlyList<WeatherReport>> GetMinHumidityReportsAsync(IQueryable<WeatherReport> reports, float humidityThreshold, CancellationToken cancellationToken = default)
     {
-        return await reports.Where(report => report.Humidity >= humidityThreshold).ToListAsync();
+        return await reports.Where(report => report.Humidity >= humidityThreshold).ToListAsync(cancellationToken);
     }
 }
