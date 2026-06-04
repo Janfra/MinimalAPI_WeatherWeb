fba7b61a614f88d617716611ae20fc862ab35ec2
Jan <janfrancoherrera@gmail.com>
2026-06-04 20:14:54 +0100

    feat: add weather service configurator
    # Description
    Added an extension method to the service collection to add the services required by the weather API.
    
    Added the new `LocationValidator` to the services.

---
+++ src/WeatherWeb.API/ServiceConfigurators/WeatherServiceConfigurator.cs
@@ -0,0 +1,23 @@
+﻿namespace WeatherWeb.ServiceConfigurators;
+
+using FluentValidation;
+using WeatherWeb.Data;
+using WeatherWeb.Models;
+using WeatherWeb.Services.Formatter;
+using WeatherWeb.Services.Reporter;
+using WeatherWeb.Validators;
+
+public static class WeatherServiceConfigurator
+{
+    public static void AddWeatherServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IWeatherFormatter, StandardWeatherFormatter>();
+        services.AddScoped<IWeatherReporter, WeatherReporter>();
+        services.AddScoped<LocationValidator>();
+        services.AddDbContext<WeatherDbContext>();
+        services.AddScoped<IWeatherDbContext>(
+            serviceProvider => serviceProvider.GetRequiredService<WeatherDbContext>()
+        );
+        services.AddScoped<IValidator<WeatherReportDTO>, WeatherReportDTOValidator>();
+    }
+}
