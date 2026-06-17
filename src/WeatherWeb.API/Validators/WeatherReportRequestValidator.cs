171939fbaafd67fe69fcf409cee4f2f90e26b65a
Jan <janfrancoherrera@gmail.com>
2026-06-17 11:42:26 +0100

    feat: add weather report request validator
    # Description
    Implemented weather report request validator using `FluentValidation`.

---
+++ src/WeatherWeb.API/Validators/WeatherReportRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace WeatherWeb.Validators;
+
+using FluentValidation;
+using WeatherWeb.Models;
+
+public class WeatherReportRequestValidator : AbstractValidator<WeatherReportRequest>
+{
+    public WeatherReportRequestValidator()
+    {
+        RuleFor(report => report.TemperatureC).SetValidator(new TemperatureCValidator());
+        RuleFor(report => report.Humidity).SetValidator(new HumidityValidator());
+        RuleFor(report => report.Location).SetValidator(new LocationValidator());
+    }
+}
