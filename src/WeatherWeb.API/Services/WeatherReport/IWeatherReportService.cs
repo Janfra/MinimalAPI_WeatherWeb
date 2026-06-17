bc18dcc8a69d3a8ec8216217fa5875936b5427df
Jan <janfrancoherrera@gmail.com>
2026-06-17 11:37:17 +0100

    feat: implement weather report service
    # Description
    Implemented weather report service that will replace the `IWeatherReporter`, utilising only DTOs and removing the need of handling database operations, abstracting the operations to be performed by the implementation of the service.

---
+++ src/WeatherWeb.API/Services/WeatherReport/IWeatherReportService.cs
@@ -0,0 +1,14 @@
+﻿namespace WeatherWeb.Services.Reporter;
+
+using WeatherWeb.Models;
+
+public interface IWeatherReportService
+{
+    public Task<WeatherReportResponse> AddWeatherReportAsync(WeatherReportRequest weatherReportRequest, CancellationToken cancellationToken = default);
+    public Task<WeatherReportResponse?> PutWeatherReportAsync(int id, WeatherReportRequest weatherReportRequest, CancellationToken cancellationToken = default);
+    public Task<WeatherReportResponse?> DeleteWeatherReportAsync(int id, CancellationToken cancellationToken = default);
+    public Task<IReadOnlyList<WeatherReportResponse>> GetHotReportsAsync(CancellationToken cancellationToken = default);
+    public Task<IReadOnlyList<WeatherReportResponse>> GetLocationReportsAsync(string location, CancellationToken cancellationToken = default);
+    public Task<IReadOnlyList<string>> GetFormattedReportsAsync(CancellationToken cancellationToken = default);
+    public Task<IReadOnlyList<WeatherReportResponse>> GetMinHumidityReportsAsync(float humidityThreshold, CancellationToken cancellationToken = default);
+}
---
+++ src/WeatherWeb.API/Services/WeatherReport/WeatherReportService.cs
@@ -0,0 +1,62 @@
+﻿namespace WeatherWeb.Services.Reporter;
+
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WeatherWeb.Data;
+using WeatherWeb.Models;
+using WeatherWeb.Services.Formatter;
+
+public class WeatherReportService(IWeatherFormatter formatter, IWeatherDbContext weatherDb) : IWeatherReportService
+{
+    public const int HOT_THRESHOLD_DEGREES = 30;
+    private readonly IWeatherFormatter _formatter = formatter;
+    private readonly IWeatherDbContext _weatherDb = weatherDb;
+
+    public async Task<WeatherReportResponse?> PutWeatherReportAsync(int id, WeatherReportRequest weatherReportRequest)
+    {
+        var report = await _weatherDb.WeatherReports.FindAsync(id);
+        if (report == null)
+        {
+            return null;
+        }
+
+        report.TemperatureC = weatherReportRequest.TemperatureC;
+        report.Humidity = weatherReportRequest.Humidity;
+        report.Location = weatherReportRequest.Location;
+        await _weatherDb.SaveChangesAsync();
+        return report.ToResponse();
+    }
+
+    public async Task<WeatherReportResponse?> DeleteWeatherReportAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var report = await _weatherDb.WeatherReports.FindAsync(id);
+        if (report == null)
+        {
+            return null;
+        }
+
+        _weatherDb.Remove(report);
+        await _weatherDb.SaveChangesAsync();
+        return report.ToResponse(); 
+    }
+
+    public async Task<IReadOnlyList<string>> GetFormattedReportsAsync(CancellationToken cancellationToken = default)
+    {
+        return await _weatherDb.WeatherReports.Select(report => _formatter.Format(report)).ToListAsync(cancellationToken);
+    }
+
+    public async Task<IReadOnlyList<WeatherReportResponse>> GetHotReportsAsync(CancellationToken cancellationToken = default)
+    {
+        return await _weatherDb.WeatherReports.Where(report => report.TemperatureC >= HOT_THRESHOLD_DEGREES).Select(report => report.ToResponse()).ToListAsync(cancellationToken);
+    }
+
+    public async Task<IReadOnlyList<WeatherReportResponse>> GetLocationReportsAsync(string location, CancellationToken cancellationToken = default)
+    {
+        return await _weatherDb.WeatherReports.Where(report => report.Location.Equals(location)).Select(report => report.ToResponse()).ToListAsync(cancellationToken);
+    }
+
+    public async Task<IReadOnlyList<WeatherReportResponse>> GetMinHumidityReportsAsync(float humidityThreshold, CancellationToken cancellationToken = default)
+    {
+        return await _weatherDb.WeatherReports.Where(report => report.Humidity >= humidityThreshold).Select(report => report.ToResponse()).ToListAsync(cancellationToken);
+    }
+}
