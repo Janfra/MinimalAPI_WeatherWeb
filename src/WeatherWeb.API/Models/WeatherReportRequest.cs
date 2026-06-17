e81451f72755c79ef1f42192ee82bb190f1c8345
Jan <janfrancoherrera@gmail.com>
2026-06-17 11:26:11 +0100

    feat: introduce requests and response DTO
    # Description
    Replaced `WeatherReportDTO` with two separate DTOs to represent API input and output, `WeatherReportRequest` and `WeatherReportResponse` respectively.

--- src/WeatherWeb.API/Models/WeatherReportRequest.cs
+++ src/WeatherWeb.API/Models/WeatherReportRequest.cs
@@ -2,13 +2,10 @@
 
 using System.Text.Json.Serialization;
 
-public record WeatherReportDTO(
+public record WeatherReportRequest(
     [property: JsonPropertyName("temp_c")] int TemperatureC,
     [property: JsonPropertyName("humidity_percent")] float Humidity,
     [property: JsonPropertyName("location")] string Location)
 {
-    [JsonPropertyName("temp_f")]
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-    public WeatherReport ToEntity() => new WeatherReport(TemperatureC, Humidity, Location);
+    public WeatherReportRequest ToEntity() => new WeatherReportRequest(TemperatureC, Humidity, Location);
 }
---
+++ src/WeatherWeb.API/Models/WeatherReportResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace WeatherWeb.Models;
+
+using System.Text.Json.Serialization;
+
+public record WeatherReportResponse(
+    [property: JsonPropertyName("id")] int Id,
+    [property: JsonPropertyName("temp_c")] int TemperatureC,
+    [property: JsonPropertyName("humidity_percent")] float Humidity,
+    [property: JsonPropertyName("location")] string Location)
+{
+    [JsonPropertyName("temp_f")]
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public static WeatherReportResponse FromEntity(WeatherReport entity) => new(entity.Id, entity.TemperatureC, entity.Humidity, entity.Location);
+}
